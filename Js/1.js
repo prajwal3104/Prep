377db375559bbdddece67445c498fb84bacbe45c
prajwal3104 <Prajwal0836@gmail.com>
2024-01-30 01:12:32 +0530

    update prep

---
+++ Js/1.js
@@ -0,0 +1,19 @@
+var myName = "Prajju"; // Global variable
+
+myName = Prajwal; // Global variable can be changed anywhere
+
+let ourName = "family"; // Local variable or scope variable where it is declared
+
+const pi = 3.14; // Constant variable which cannot be changed
+
+var a; // Declaring variable
+var b = 2; // Declaring and assigning variable
+console.log(a); 
+// op: Null
+
+a = 6;
+
+b = a;
+
+console.log(a);
+// op: 6
