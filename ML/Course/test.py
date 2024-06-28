ac5f31d2c5f0a8b6a1c186d2970a54dd7a81daff
prajwal3104 <prajwal0836@gmail.com>
2024-06-28 23:02:40 +0530

    Updated code

--- ML/Course/test.py
+++ ML/Course/test.py
@@ -15,7 +15,10 @@ y = data['label_num']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 nb_classifier = MultinomialNB()
-nb_classifier.fit(X_train, y_train)
+nb_classifier.fit(X_train, y_train) # this is 
+
+
+
 
 y_pred = nb_classifier.predict(X_test)
 
